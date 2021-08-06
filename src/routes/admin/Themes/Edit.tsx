8333f94aa7f76bf5093d432989828ffbc05be991
gitquil07 <gitquil07@gmail.com>
2021-08-06 19:04:02 +0500

    changes

--- src/routes/admin/Themes/Edit.tsx
+++ src/routes/admin/Themes/Edit.tsx
@@ -3,14 +3,34 @@ import { GoBack } from "components/General";
 import { FilterSelect } from "components/Inputs";
 import { RegularInput } from "components/Inputs";
 import { FormAddButton, FormCloseButton } from "components/Buttons";
-import { RowGroup, InlineGroup } from "components/General/Wrappers";
+import { RowGroup, InlineGroup, BottomOffset } from "components/General/Wrappers";
+import { Form } from "components/General/Form";
+import { InputGroupHeader } from "components/General/Wrappers";
 
 const Edit = () => {
 
+
     return (
+        <>
+            <BottomOffset>
+                <GoBack text="Назад" />
+            </BottomOffset>
+            <PageHeader title="Добавить тему" alignment="flex-start" />
+            <Form>
+                <InlineGroup>
+                    <FilterSelect name="system"  handleChange={() => {}}>
 
-        <p>Edit</p>
+                    </FilterSelect>
+                    <FilterSelect name="directions" handleChange={() => {}}>
 
+                    </FilterSelect>
+                </InlineGroup>
+                <RowGroup>
+                    <InputGroupHeader title="Название темы" />
+                    <RegularInput name="theme" value="" placeholder="" />
+                </RowGroup>
+            </Form>
+        </>
     );
 
 }
